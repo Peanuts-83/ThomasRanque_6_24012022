25e9d6a12880caf691354c48bb666cad4062681c
Peanuts-83 <tranque@free.fr>
2022-01-24 18:55:49 +0100

    index.js LOAD JSON ok && factories>photographers.js FULL ARTICLE ok

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,33 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
-
+    const { name, id, city, country, tagline, price, portrait } = data;
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+
+        // ARTICLE HAUT + <a></a>
+        const link = document.createElement('a');
+        link.href = id;
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        link.appendChild(img);
+        link.appendChild(h2);
+
+        // ARTICLE BAS
+        const address = document.createElement('address');
+        const blockquote = document.createElement('blockquote');
+        const data = document.createElement('data');
+        address.innerText = `${city}, ${country}`;
+        blockquote.innerText = `${tagline}`;
+        data.innerText = `${price}€/jour`;
+
+        // ASSEMBLAGE ARTICLE
+        const elements = [link, address, blockquote, data];
+        elements.forEach(element => article.appendChild(element));
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+
+    return { name, picture, getUserCardDOM };
 }
